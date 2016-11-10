fff07a97c936e8d1b5652c49696e6881744139e7
Jjennings239 <ellystrwberry@gmail.com>
2016-11-10 15:51:31 -0600

    Update masking.java

--- src/masking.java
+++ src/masking.java
@@ -9,46 +9,27 @@ import java.util.ArrayList;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-public class masking extends JFrame {
+public class Masking extends JFrame {
 
-    public static void main(String[] args) {
-        new masking();
-    }
-
-    public masking() {
+    public masking(ArrayList<Polygon> polys) {
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(new PaintSurface(), BorderLayout.CENTER);
+        this.add(new PaintSurface(polys), BorderLayout.CENTER);
         this.setVisible(true);
     }
 
     private class PaintSurface extends JComponent {
         ArrayList<Polygon> shapes = new ArrayList<Polygon>();
-        int numOfShapes = 4;
-        int xPoints[][] = {{200,250, 150},
-                {175, 225, 125},
-                {150, 200, 100},
-                {225, 275, 175}};
-        int yPoints [][] = {{200, 150, 150},
-                {175, 125, 125},
-                {150, 100, 100},
-                {225, 175, 175}};
 
-        public PaintSurface() {
-            for(int i = 0; i < numOfShapes; i++){
-                Polygon poly = new Polygon();
-                for(int j = 0; j < 3; j++){
-                    poly.addPoint(xPoints[i][j], yPoints[i][j]);
-                }
-                shapes.add(i, poly);
-            }
+        public PaintSurface(ArrayList<Polygon> polys) {
+            shapes = polys;
             repaint();
         }
         private void paintBackground(Graphics2D g2){
             g2.setPaint(Color.WHITE);
 
         }
-        public void paint(Graphics g) {
+    public void paint(Graphics g) {
             Graphics2D g2 = (Graphics2D) g;
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             paintBackground(g2);
@@ -66,9 +47,5 @@ public class masking extends JFrame {
                 g2.fill(p); //fill
             }
         }
-
-        private Rectangle2D.Float makeRectangle(int x1, int y1, int x2, int y2) {
-            return new Rectangle2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
         }
-    }
 }
